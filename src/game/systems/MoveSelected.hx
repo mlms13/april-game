80ac56799546459941086db6632aa5852ed59000
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-04-10 22:13:25 -0600

    Move orange square when you click it

--- src/game/systems/MoveSelected.hx
+++ src/game/systems/MoveSelected.hx
@@ -3,12 +3,15 @@ package game.systems;
 import edge.ISystem;
 import game.components.Selected;
 import game.components.Display;
+import game.components.Position;
 
 class MoveSelected implements ISystem {
   public function new() {
   }
 
-  public function update(s : Selected, d : Display) {
+  public function update(s : Selected, d : Display, p : Position) {
     d.sprite.alpha = 1;
+    p.x += 0.01;
+    p.y += 0.01;
   }
 }
