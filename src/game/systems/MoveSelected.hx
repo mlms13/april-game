f79ce1f7e08a2f44467b9a580e4dc46e8e9adc42
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-04-25 23:57:09 -0600

    Rotate tank toward target point

--- src/game/systems/MoveSelected.hx
+++ src/game/systems/MoveSelected.hx
@@ -10,9 +10,10 @@ class MoveSelected implements ISystem {
   public function new() {
   }
 
-  public function update(s : Selected, d : Display, p : Position, t : Target) {
+  public function update(s : Selected, d : Display, p : Position, r : Rotation, t : Target) {
     d.node.alpha = 1;
 
+    r.angle = Math.atan2(t.y - p.y, t.x - p.x);
 
     p.x += 0.01 * (t.x - p.x);
     p.y += 0.01 * (t.y - p.y);
